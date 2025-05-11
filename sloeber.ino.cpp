2c36aabf872401420014e49bb4fb028218cb782e
jp112sdl <>
2025-05-11 22:18:20 +0200

    Update project env

---
+++ sloeber.ino.cpp
@@ -0,0 +1,28 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2025-04-13 20:39:14
+
+#include "Arduino.h"
+#define LOGLEVEL 0
+#include <ArduinoOTA.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266WebServer.h>
+#include "printf_helper.h"
+#include "Em7seg_cntrl.h"
+extern EM7ModuleArray d;
+#include "WiFiFunctions.h"
+#include "NtpFunctions.h"
+
+void startOTAhandling() ;
+void rotateAnimation() ;
+void setup() ;
+void showtime(uint8_t h, uint8_t m) ;
+void loop() ;
+
+#include "EM7SEG.ino"
+
+
+#endif
---
+++ spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ /Users/pechj/Documents/sloeber-workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
